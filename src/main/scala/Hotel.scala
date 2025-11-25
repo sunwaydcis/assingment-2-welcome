4e843e55cd2f41fb2be2943dd182600d489f2a8a
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2025-11-25 14:44:17 +0800

    Branching code to my own branch

--- src/main/scala/Hotel.scala
+++ src/main/scala/Hotel.scala
@@ -1,12 +1,20 @@
 import com.github.tototoshi.csv._
 
 object CsvReader {
-  def main(args: Array[String]): Unit = {
+  def read(): Unit =
     val reader = CSVReader.open("src/main/resources/Hotel_Dataset.csv")
     val rows: List[Map[String, String]] = reader.allWithHeaders()
     reader.close()
 
     println(rows.head)
     println(rows.length)
-  }
+  end read
 }
+
+
+
+
+
+object Main extends App:
+  CsvReader.read()
+end Main
