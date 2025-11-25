01b85caba635eb6ff61b6d38cca683337fa6d00c
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2025-11-25 15:40:20 +0800

    backup of finishing question 1

--- src/main/scala/Hotel.scala
+++ src/main/scala/Hotel.scala
@@ -1,26 +1,21 @@
-import com.github.tototoshi.csv._
+import CsvReader.rows
+import com.github.tototoshi.csv.*
 
 object CsvReader {
   val reader = CSVReader.open("src/main/resources/Hotel_Dataset.csv")
   val rows: List[Map[String, String]] = reader.allWithHeaders()
 
-  def read(): Unit =
-    val reader = CSVReader.open("src/main/resources/Hotel_Dataset.csv")
-    val rows: List[Map[String, String]] = reader.allWithHeaders()
-    reader.close()
-
-    println(rows.head)
-    println(rows.length)
-  end read
   def stop(): Unit = reader.close()
 }
 
 class BookCount:
   val destinatedCountry: List[String] = rows.map(row => row("Destination Country"))
   val countryCount: Map[String, Int] = destinatedCountry.groupBy(identity).view.mapValues(_.size).toMap
+
   def highestBookingCount(): Unit = println(countryCount.head)
 end BookCount
 
 object Main extends App:
-
+  val question1 = new BookCount
+  question1.highestBookingCount()
 end Main
