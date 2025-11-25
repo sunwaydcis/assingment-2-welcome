360a5c7ea7ed20e19198c3151de3ce7d9dfd3994
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2025-11-25 15:56:42 +0800

    initializing logic for question 3

--- src/main/scala/Hotel.scala
+++ src/main/scala/Hotel.scala
@@ -16,6 +16,19 @@ class BookCount:
   def highestBookingCount(): Unit = println(sortedCountryCount.maxBy(_._2))
 end BookCount
 
+class MaxProfit:
+  val hotel: List[String] = rows.map(row => row("Hotel Name"))
+  val hotelCount: Map[String, Int] = hotel.groupBy(identity).view.mapValues(_.size).toMap
+  val sortedHotelCount = ListMap(hotelCount.toSeq.sortBy(_._2): _*)
+
+  val profitMargin: List[String] = rows.map(row => row("Profit Margin"))
+  val profitMarginCount: Map[String, Int] = profitMargin.groupBy(identity).view.mapValues(_.size).toMap
+  val sortedProfitMarginCount = ListMap(profitMarginCount.toSeq.sortBy(_._2): _*)
+
+  def highestHotelCount(): Unit = println(sortedHotelCount.maxBy(_._2))
+  def highestProfitMarginCount(): Unit = println(sortedProfitMarginCount.maxBy(_._2))
+end MaxProfit
+
 object Main extends App:
   val question1 = new BookCount
   question1.highestBookingCount()
