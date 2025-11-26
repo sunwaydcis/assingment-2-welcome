aad307f45d3b4b0eecd16c708bfa5c6aadb766ec
tan-li-yang <143149789+Tan-Li-Yang@users.noreply.github.com>
2025-11-26 23:09:09 +0800

    the base of algorithm logic for question 2 (copy pasted from question 3)

--- src/main/scala/Hotel.scala
+++ src/main/scala/Hotel.scala
@@ -17,6 +17,26 @@ class BookCount:
   def highestBookingCount(): Unit = println(sortedCountryCount.maxBy(_._2))
 end BookCount
 
+class MaxEconomic:
+  //is the booking price discounted already or prediscount
+  //if so, then we use (booking price / 1 + discount) * profit margin
+  val filteredList: List[Map[String, String]] = rows.map { row =>
+    row.filter { case (key, _) => List("Hotel Name", "Booking Price[SGD]", "Discount", "Profit Margin").contains(key) }
+  }
+  val groupedList: Map[String, List[Map[String, String]]] = filteredList.groupBy(row => row("Hotel Name"))
+  var listOfEconomicalHotel: List[Map[String, Double]] = List()
+  for ((hotelName, dataRows) <- groupedList) {
+    val economicRanking = dataRows.map { row =>
+      (row("Booking Price[SGD]").toDouble / (1 + row("Discount").stripSuffix("%").toDouble / 100)) * row("Profit Margin").toDouble
+    }.sum
+    val _economicHotel: Map[String, Double] = Map(hotelName -> economicRanking)
+    listOfEconomicalHotel = listOfEconomicalHotel :+ _economicHotel
+  }
+  val sortedEconomicHotel = ListMap(listOfEconomicalHotel.map(_.head).toSeq.sortBy(_._2): _*)
+
+  def mostEconomicalHotel(): Unit = println(listOfEconomicalHotel.map(_.head).minBy(_._2))
+end MaxEconomic
+
 class MaxProfit:
   //most profitable logic is sum of (visitor[default price of 100SGD] * profit margin) group by each hotel due to not considering booking price
   //if count booking price, then can sum of (booking price * profit margin) for each visitor then group by hotel to get most profitable hotel
